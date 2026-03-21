f1e5db734cc04326c8d973484d234f384cefcdc9
Pavel Černý <htpavel@seznam.cz>
2026-03-21 19:20:48 +0100

    Added temperature measurement Data sending time set to 5 minutes.

--- core-incubator/src/application.c
+++ core-incubator/src/application.c
@@ -1,9 +1,9 @@
 /**
  * @file application.c
- * @brief Firmware pro monitorování teploty v inkubátoru (IoT uzel).
+ * @brief Firmware pro monitorování teploty a baterie v inkubátoru (IoT uzel).
  * @author Pavel Černý, Barbora Šimková, Štěpán Hruška, Tereza Holubcová, Václav Haba
- * @version 1.0
- * * Tento kód inicializuje teploměr TMP112 a odesílá naměřená data
+ * @version 1.1
+ * * Tento kód inicializuje teploměr TMP112 a modul baterie. Odesílá naměřená data
  * v pravidelných intervalech přes rádiový modul do brány (Gateway).
  */
 
@@ -16,28 +16,47 @@ twr_led_t led;
 twr_tmp112_t tmp112;
 
 /**
+ * @brief Callback funkce, která obsluhuje události z modulu baterie.
+ * @param event Typ události (aktualizace napětí).
+ * @param event_param Volitelný parametr.
+ */
+void battery_event_handler(twr_module_battery_event_t event, void *event_param)
+{
+    // Reagujeme na aktualizaci naměřeného napětí
+    if (event == TWR_MODULE_BATTERY_EVENT_UPDATE)
+    {
+        float voltage;
+        
+        // Přečtení aktuálního napětí baterie
+        if (twr_module_battery_get_voltage(&voltage))
+        {
+            // Výpis do ladicí konzole
+            twr_log_debug("APP: Napětí baterie: %.2f V", voltage);
+
+            // Odeslání hodnoty napětí rádiem
+            twr_radio_pub_battery(&voltage);
+        }
+    }
+}
+
+/**
  * @brief Callback funkce, která obsluhuje události z teploměru.
- * * @param self Ukazatel na instanci teploměru, která událost vyvolala.
- * @param event Typ události (např. aktualizace dat).
- * @param event_param Volitelný parametr předaný při inicializaci.
+ * @param self Ukazatel na instanci teploměru.
+ * @param event Typ události.
+ * @param event_param Volitelný parametr.
  */
 void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *event_param)
 {
-    // Reagujeme pouze na událost aktualizace naměřených dat
     if (event == TWR_TMP112_EVENT_UPDATE)
     {
         float celsius;
         
-        // Přečtení aktuální teploty v stupních Celsia
         if (twr_tmp112_get_temperature_celsius(self, &celsius))
         {
-            // Výpis hodnoty do ladicí konzole (Debugger)
             twr_log_debug("APP: Teplota naměřena: %.2f °C", celsius);
 
-            // Odeslání hodnoty rádiem na zadaném kanálu
             twr_radio_pub_temperature(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_ALTERNATE, &celsius);
             
-            // Krátké bliknutí LED pro indikaci úspěšného odeslání dat
             twr_led_pulse(&led, 50);
         }
     }
@@ -45,36 +64,31 @@ void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *ev
 
 /**
  * @brief Hlavní inicializační funkce aplikace. Volá se jednou po startu.
- * Nastavuje hardware a plánuje první úlohy.
  */
 void application_init(void)
 {
-    // Inicializace subsystému pro logování (výpisy do konzole)
     twr_log_init(TWR_LOG_LEVEL_DUMP, TWR_LOG_TIMESTAMP_ABS);
 
-    // Konfigurace systémové LED diody (výchozí stav vypnuto)
     twr_led_init(&led, TWR_GPIO_LED, false, 0);
-    // Úvodní bliknutí signalizující start zařízení
     twr_led_pulse(&led, 2000);
 
-    // Inicializace senzoru TMP112 na sběrnici I2C0 s adresou 0x49
+    // Inicializace senzoru TMP112
     twr_tmp112_init(&tmp112, TWR_I2C_I2C0, 0x49);
-    // Registrace obslužné funkce pro události ze senzoru
     twr_tmp112_set_event_handler(&tmp112, tmp112_event_handler, NULL);
-    
-    // Nastavení intervalu měření a odesílání na 10 000 ms (10 sekund)
-    twr_tmp112_set_update_interval(&tmp112, 10000);
+    twr_tmp112_set_update_interval(&tmp112, 300000); // 5 minut
 
-    // Aktivace rádiového přenosu v energeticky úsporném režimu
+    // Inicializace modulu baterie
+    twr_module_battery_init();
+    twr_module_battery_set_event_handler(battery_event_handler, NULL);
+    twr_module_battery_set_update_interval(3600000); // 1 hodina (šetří energii)
+
     twr_radio_init(TWR_RADIO_MODE_NODE_SLEEPING);
     
-    // Odeslání párovacího požadavku se jménem projektu
     twr_radio_pairing_request("inkubator-projekt", FW_VERSION);
 }
 
 /**
  * @brief Periodicky volaná funkce pro naplánované úlohy.
- * V tomto projektu se nevyužívá, protože veškerou logiku řídí události (events).
  */
 void application_task(void)
 {
